28603fed790a9d30b0abb9964345673695b2aec1
Marcin Bober <mbober1@gmail.com>
2021-04-25 17:37:40 +0200

    description

--- src/connectiondialog.cpp
+++ src/connectiondialog.cpp
@@ -1,6 +1,11 @@
 #include "connectiondialog.h"
 #include "ui_connectiondialog.h"
 
+
+/**
+ * A constructor.
+ * @param parent QWidget type parent.
+ */
 ConnectionDialog::ConnectionDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ConnectionDialog)
@@ -11,21 +16,38 @@ ConnectionDialog::ConnectionDialog(QWidget *parent) :
     ui->lineEditAdress->setInputMask("900.900.900.900");
 }
 
+
+/**
+ * A destructor.
+ */
 ConnectionDialog::~ConnectionDialog()
 {
     delete ui;
 }
 
 
+/**
+ * Get adress for connection.
+ * @return IP address of server.
+ */
 QString ConnectionDialog::getAdress() {
     return ui->lineEditAdress->text();
 }
 
 
+/**
+ * Get UDP port
+ * @return UDP server port.
+ */
 int ConnectionDialog::getUdpPort() {
     return ui->lineEditUdpPort->text().toInt();
 }
 
+
+/**
+ * Get TCP port
+ * @return TCP server port.
+ */
 int ConnectionDialog::getTcpPort() {
     return ui->lineEditTcpPort->text().toInt();
 }
