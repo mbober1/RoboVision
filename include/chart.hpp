afec202084773248db93093dfb50bc993aa093f6
Marcin Bober <mbober1@gmail.com>
2021-04-29 13:13:33 +0200

    linker error

---
+++ include/chart.hpp
@@ -0,0 +1,51 @@
+#ifndef CHART_H
+#define CHART_H
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+#include <QtCharts/QDateTimeAxis>
+#include <QtCharts/QValueAxis>
+#include <QDateTime>
+#include <QtCharts/QChart>
+#include <QtCore/QTimer>
+
+QT_CHARTS_BEGIN_NAMESPACE
+class QSplineSeries;
+class QValueAxis;
+QT_CHARTS_END_NAMESPACE
+
+QT_CHARTS_USE_NAMESPACE
+
+class Chart : public QChart
+{
+    Q_OBJECT
+public:
+    Chart(QGraphicsItem *parent = nullptr, Qt::WindowFlags wFlags = {});
+
+public slots:
+    void addPoint(int x, int y, int z);
+    void timerTimeout();
+
+private:
+    QTimer timer;
+
+    QSplineSeries *x_series;
+    QSplineSeries *y_series;
+    QSplineSeries *z_series;
+    QStringList m_titles;
+    QValueAxis *m_axisX;
+    QValueAxis *m_axisY;
+    qreal m_x;
+    qreal m_y;
+
+    uint pointCount;
+};
+
+#endif
+
+
+
+
+
