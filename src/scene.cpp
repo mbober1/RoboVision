dcab9252fe049ee9e74ea5d5adbdcc5dece68554
Marcin Bober <mbober1@gmail.com>
2021-04-30 00:40:35 +0200

    fix

--- src/scene.cpp
+++ src/scene.cpp
@@ -17,35 +17,40 @@ QWidget(parent)
     Qt3DRender::QMaterial *material = new Qt3DExtras::QPhongMaterial(rootEntity);
 
     // Torus
-    // Qt3DCore::QEntity *torusEntity = new Qt3DCore::QEntity(rootEntity);
-    // Qt3DExtras::QTorusMesh *torusMesh = new Qt3DExtras::QTorusMesh;
-    // torusMesh->setRadius(5);
-    // torusMesh->setMinorRadius(1);
-    // torusMesh->setRings(100);
-    // torusMesh->setSlices(20);
+    Qt3DCore::QEntity *torusEntity = new Qt3DCore::QEntity(rootEntity);
+    Qt3DExtras::QTorusMesh *torusMesh = new Qt3DExtras::QTorusMesh;
+    torusMesh->setRadius(5);
+    torusMesh->setMinorRadius(1);
+    torusMesh->setRings(100);
+    torusMesh->setSlices(20);
 
-    // Qt3DCore::QTransform *torusTransform = new Qt3DCore::QTransform;
-    // torusTransform->setScale3D(QVector3D(1.5, 1, 0.5));
-    // torusTransform->setRotation(QQuaternion::fromAxisAndAngle(QVector3D(1, 0, 0), 45.0f));
+    Qt3DCore::QTransform *torusTransform = new Qt3DCore::QTransform;
+    torusTransform->setScale3D(QVector3D(1.5, 1, 0.5));
+    torusTransform->setRotation(QQuaternion::fromAxisAndAngle(QVector3D(1, 0, 0), 45.0f));
 
-    // torusEntity->addComponent(torusMesh);
-    // torusEntity->addComponent(torusTransform);
-    // torusEntity->addComponent(material);
+    torusEntity->addComponent(torusMesh);
+    torusEntity->addComponent(torusTransform);
+    torusEntity->addComponent(material);
 
     // Sphere
     Qt3DCore::QEntity *sphereEntity = new Qt3DCore::QEntity(rootEntity);
     Qt3DExtras::QSphereMesh *sphereMesh = new Qt3DExtras::QSphereMesh;
     sphereMesh->setRadius(3);
     sphereMesh->setGenerateTangents(true);
-
     Qt3DCore::QTransform *sphereTransform = new Qt3DCore::QTransform;
-
     sphereEntity->addComponent(sphereMesh);
     sphereEntity->addComponent(sphereTransform);
     sphereEntity->addComponent(material);
 
+
+    // Chassis
+    // Qt3DExtras::QSphereMesh *sphereMesh = new Qt3DExtras::QSphereMesh;
+    // object->transform()->setScale3D(QVector3D(1.5, 1, 0.5));
+    // object->setRotation(QQuaternion::fromAxisAndAngle(QVector3D(1, 0, 0), 45.0f));
+    // object->addComponent(sphereMesh);
+
     this->object = new Object(this->rootEntity);
-    this->object->loader()->setSource(QUrl(QString("file:/home/mbober/Documents/RoboVision/chassis.stl")));
+    this->object->loader()->setSource(QUrl(QString("file:../chassis.stl")));
     view->setRootEntity(rootEntity);
 
 }
