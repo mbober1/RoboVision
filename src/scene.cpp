62771281f0deb431a0e8f6c869d5214f2ae0bab4
Marcin Bober <mbober1@gmail.com>
2021-05-08 23:26:43 +0200

    cleanup

--- src/scene.cpp
+++ src/scene.cpp
@@ -7,56 +7,48 @@ QWidget(parent)
     Qt3DExtras::Qt3DWindow *view = new Qt3DExtras::Qt3DWindow();
     container = QWidget::createWindowContainer(view);
     container->setMinimumSize(QSize(400, 400));
-    rootEntity = new Qt3DCore::QEntity();
+
+    view->defaultFrameGraph()->setClearColor(QColor(49, 54, 59));
+
+    Qt3DCore::QEntity *rootEntity = new Qt3DCore::QEntity;
+    QUrl data = QUrl::fromLocalFile("/home/mbober/Documents/RoboVision/Model.obj");
 
     camera = view->camera();
     camera->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
     camera->setPosition(QVector3D(0, 0, 40.0f));
-    camera->setViewCenter(QVector3D(0, 0, 0));
+    camera->setViewCenter(QVector3D(0, 0, 0));    
 
-    Qt3DRender::QMaterial *material = new Qt3DExtras::QPhongMaterial(rootEntity);
+    Qt3DCore::QEntity *lightEntity = new Qt3DCore::QEntity(rootEntity);
+    Qt3DRender::QPointLight *light = new Qt3DRender::QPointLight(lightEntity);
+    light->setColor("white");
+    light->setIntensity(0.8f);
+    lightEntity->addComponent(light);
 
-    // Torus
-    Qt3DCore::QEntity *torusEntity = new Qt3DCore::QEntity(rootEntity);
-    Qt3DExtras::QTorusMesh *torusMesh = new Qt3DExtras::QTorusMesh;
-    torusMesh->setRadius(5);
-    torusMesh->setMinorRadius(1);
-    torusMesh->setRings(100);
-    torusMesh->setSlices(20);
+    Qt3DCore::QTransform *lightTransform = new Qt3DCore::QTransform(lightEntity);
+    lightTransform->setTranslation(QVector3D(60, 0, 40.0f));
+    lightEntity->addComponent(lightTransform);
 
-    Qt3DCore::QTransform *torusTransform = new Qt3DCore::QTransform;
-    torusTransform->setScale3D(QVector3D(1.5, 1, 0.5));
-    torusTransform->setRotation(QQuaternion::fromAxisAndAngle(QVector3D(1, 0, 0), 45.0f));
+    Qt3DExtras::QOrbitCameraController *camController = new Qt3DExtras::QOrbitCameraController(rootEntity);
+    camController->setCamera(camera);
 
-    torusEntity->addComponent(torusMesh);
-    torusEntity->addComponent(torusTransform);
-    torusEntity->addComponent(material);
+
+    Qt3DExtras::QPhongMaterial *material = new Qt3DExtras::QPhongMaterial();
+    material->setDiffuse(QColor(255, 0, 0));
+
+    Qt3DCore::QEntity *robot = new Qt3DCore::QEntity(rootEntity);
+    Qt3DRender::QMesh *robotMesh = new Qt3DRender::QMesh;
+    robotMesh->setSource(data);
+    robot->addComponent(robotMesh);
+    robot->addComponent(material);
 
     // Sphere
     Qt3DCore::QEntity *sphereEntity = new Qt3DCore::QEntity(rootEntity);
     Qt3DExtras::QSphereMesh *sphereMesh = new Qt3DExtras::QSphereMesh;
     sphereMesh->setRadius(3);
-    sphereMesh->setGenerateTangents(true);
-    Qt3DCore::QTransform *sphereTransform = new Qt3DCore::QTransform;
     sphereEntity->addComponent(sphereMesh);
-    sphereEntity->addComponent(sphereTransform);
     sphereEntity->addComponent(material);
 
 
-    // Chassis
-    // Qt3DExtras::QSphereMesh *sphereMesh = new Qt3DExtras::QSphereMesh;
-    // object->transform()->setScale3D(QVector3D(1.5, 1, 0.5));
-    // object->setRotation(QQuaternion::fromAxisAndAngle(QVector3D(1, 0, 0), 45.0f));
-    // object->addComponent(sphereMesh);
-
-    this->object = new Object(this->rootEntity);
-    this->object->loader()->setSource(QUrl(QString("file:../chassis.stl")));
     view->setRootEntity(rootEntity);
-
-}
-
-
-void Scene::createObject() {
-    // this->object = new Object(this->rootEntity);
-    // this->object->loader()->setSource(QUrl(QString("file:/home/mbober/Documents/RoboVision/chassis.stl")));
+    // view->show();
 }
