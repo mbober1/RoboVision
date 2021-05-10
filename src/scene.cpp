ef72ad6f5e6e3126db226510e6cf20d70bcf9ac3
Marcin Bober <mbober1@gmail.com>
2021-05-10 23:26:11 +0200

    offset center

--- src/scene.cpp
+++ src/scene.cpp
@@ -20,7 +20,7 @@ QWidget(parent), x(0), y(0), z(0)
     // camera
     camera = view->camera();
     camera->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
-    camera->setPosition(QVector3D(0, 0, 250.0f));
+    camera->setPosition(QVector3D(0, 0, 300.0f));
     camera->setViewCenter(QVector3D(0, 0, 0));  
     
     Qt3DExtras::QOrbitCameraController *camController = new Qt3DExtras::QOrbitCameraController(rootEntity);
@@ -55,7 +55,8 @@ void Scene::rotate(float x, float y, float z) {
     this->x += x;
     this->y += y;
     this->z += z;
-    QMatrix4x4 matrix_x(1, 0, 0, 0, 0, cos(this->x), -sin(this->x), 0 , 0, sin(this->x), cos(this->x), 0, 0, 0, 0, 1);
+    QVector3D offset(0, 0, 55);
+    QMatrix4x4 matrix_x(1, 0, 0, offset[0], 0, cos(this->x), -sin(this->x), offset[1] , 0, sin(this->x), cos(this->x), offset[2], 0, 0, 0, 1);
     QMatrix4x4 matrix_y(cos(this->y), 0, sin(this->y), 0, 0, 1, 0, 0, -sin(this->y), 0, cos(this->y), 0, 0, 0, 0, 1);
     QMatrix4x4 matrix_z(cos(this->z), -sin(this->z), 0, 0, sin(this->z), cos(this->z), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
     QMatrix4x4 matrix = matrix_x * matrix_y * matrix_z;
