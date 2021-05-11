60b49365bafcd3cb068e40846b453154be353e28
Marcin Bober <mbober1@gmail.com>
2021-05-11 13:26:15 +0200

    light fix

--- src/scene.cpp
+++ src/scene.cpp
@@ -31,11 +31,11 @@ QWidget(parent), x(0), y(0), z(0)
     Qt3DCore::QEntity *lightEntity = new Qt3DCore::QEntity(rootEntity);
     Qt3DRender::QPointLight *light = new Qt3DRender::QPointLight(lightEntity);
     light->setColor("red");
-    light->setIntensity(1);
+    light->setIntensity(2);
     lightEntity->addComponent(light);
 
     Qt3DCore::QTransform *lightTransform = new Qt3DCore::QTransform(lightEntity);
-    lightTransform->setTranslation(QVector3D(60, 0, 40.0f));
+    lightTransform->setTranslation(QVector3D(180, 0, 0));
     lightEntity->addComponent(lightTransform);
 
 
